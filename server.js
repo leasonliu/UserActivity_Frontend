abef4e8c3fc001f8fdb555a8712f0d98f63b8841
eason <yul196@pitt.edu>
2018-04-13 20:19:36 -0400

    publish v

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + "/www"));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
